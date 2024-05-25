7cb27f254b1d6971ac2e15987f12a180aef7e17d
usasv <uscasv@gmail.com>
2024-05-26 00:41:15 +0500

    Mapping profile

---
+++ WAD.00014384.DAL/Profiles/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using WAD._00014384.DAL.DTOs;
+using WAD._00014384.DAL.Models;
+
+namespace WAD._00014384.DAL.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Event, EventDto>()
+                .ForMember(dest => dest.Attendees, opt => opt.MapFrom(src => src.Attendees));
+
+            CreateMap<CreateEventDto, Event>();
+
+            CreateMap<UpdateEventDto, Event>();
+
+            CreateMap<Attendee, AttendeeDto>();
+
+            CreateMap<CreateAttendeeDto, Attendee>();
+
+            CreateMap<UpdateAttendeeDto, Attendee>();
+        }
+    }
+}
