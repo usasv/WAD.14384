28b650b630349a102335dcc24f25b60c05e0aee5
usasv <uscasv@gmail.com>
2024-05-26 00:40:37 +0500

    Added migration with DAL layer configuration

---
+++ WAD.00014384.DAL/ConfigurationServices.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using WAD._00014384.DAL.Data;
+using WAD._00014384.DAL.Interfaces;
+using WAD._00014384.DAL.Repositories;
+
+namespace WAD._00014384.DAL
+{
+    public static class ConfigrationDAL
+    {
+        public static IServiceCollection AddConfigurationServices(
+            this IServiceCollection services,
+            IConfiguration configuration
+        )
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IEventRepository, EventRepository>();
+            services.AddScoped<IAttendeeRepository, AttendeeRepository>();
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
