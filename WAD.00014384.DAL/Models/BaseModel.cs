e304e47ea3a7efec05172f4b25cf853b682d7a43
usasv <uscasv@gmail.com>
2024-05-26 00:39:46 +0500

    Models added

---
+++ WAD.00014384.DAL/Models/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAD._00014384.DAL.Models
+{
+    public abstract class BaseModel
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ WAD.00014384.DAL/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAD._00014384.DAL.Models
+{
+    public class Event : BaseModel
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public ICollection<Attendee> Attendees { get; set; }
+    }
+}
