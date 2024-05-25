281be0bfad7ea8c58a70db484f223698906f06c3
usasv <uscasv@gmail.com>
2024-05-26 00:40:48 +0500

    Added interfaces

---
+++ WAD.00014384.DAL/Interfaces/IEventRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WAD._00014384.DAL.Models;
+
+namespace WAD._00014384.DAL.Interfaces
+{
+    public interface IEventRepository : IBaseRepository<Event>
+    {
+
+    }
+}
