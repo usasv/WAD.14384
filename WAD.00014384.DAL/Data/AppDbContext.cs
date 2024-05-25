1c955a7f794a5eedaf79ddf299cfef3ae662b0f1
usasv <uscasv@gmail.com>
2024-05-26 00:40:17 +0500

    App db context with configuration

---
+++ WAD.00014384.DAL/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WAD._00014384.DAL.Models;
+
+namespace WAD._00014384.DAL.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Attendee> Attendees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>()
+                .HasKey(e => e.Id);
+
+            modelBuilder.Entity<Attendee>()
+                .HasKey(a => a.Id);
+
+            modelBuilder.Entity<Event>()
+                .HasMany(e => e.Attendees)
+                .WithOne(a => a.Event)
+                .HasForeignKey(a => a.EventId);
+        }
+    }
+}
