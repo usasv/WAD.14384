98733965f05ad6364f024a4ed8ae9f4a4f6cf74b
usasv <uscasv@gmail.com>
2024-05-26 00:40:57 +0500

    Developed repositories

---
+++ WAD.00014384.DAL/Repositories/BaseRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WAD._00014384.DAL.Data;
+
+namespace WAD._00014384.DAL.Interfaces
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+
+        public BaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            if (entity != null)
+            {
+                _context.Set<T>().Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ WAD.00014384.DAL/Repositories/EventRepository .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WAD._00014384.DAL.Data;
+using WAD._00014384.DAL.Interfaces;
+using WAD._00014384.DAL.Models;
+
+namespace WAD._00014384.DAL.Repositories
+{
+    public class EventRepository : BaseRepository<Event>, IEventRepository
+    {
+        public EventRepository(AppDbContext context) : base(context)
+        {
+
+        }
+    }
+}
