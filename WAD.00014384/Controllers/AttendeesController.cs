5cfa82f17e0d077c23cbc7a7caa3fdb210a05d0e
usasv <uscasv@gmail.com>
2024-05-26 00:41:43 +0500

    Controller added with all CRUD endpoints

---
+++ WAD.00014384/Controllers/AttendeesController.cs
@@ -0,0 +1,92 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WAD._00014384.DAL.DTOs;
+using WAD._00014384.DAL.Interfaces;
+using WAD._00014384.DAL.Models;
+
+namespace WAD._00014384.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttendeesController : ControllerBase
+    {
+        private readonly IAttendeeRepository _attendeeRepository;
+        private readonly IMapper _mapper;
+
+        public AttendeesController(IAttendeeRepository attendeeRepository, IMapper mapper)
+        {
+            _attendeeRepository = attendeeRepository;
+            _mapper = mapper;
+        }
+
+        // GET: api/attendees
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AttendeeDto>>> GetAttendees()
+        {
+            var attendees = await _attendeeRepository.GetAllAsync();
+            return Ok(_mapper.Map<IEnumerable<AttendeeDto>>(attendees));
+        }
+
+        // GET: api/attendees/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AttendeeDto>> GetAttendee(int id)
+        {
+            var attendee = await _attendeeRepository.GetByIdAsync(id);
+
+            if (attendee == null)
+            {
+                return NotFound();
+            }
+
+            return _mapper.Map<AttendeeDto>(attendee);
+        }
+
+        // POST: api/attendees
+        [HttpPost]
+        public async Task<ActionResult<AttendeeDto>> CreateAttendee(CreateAttendeeDto createAttendeeDto)
+        {
+            var attendee = _mapper.Map<Attendee>(createAttendeeDto);
+            await _attendeeRepository.AddAsync(attendee);
+            return Ok(_mapper.Map<AttendeeDto>(attendee));
+        }
+
+        // PUT: api/attendees/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAttendee(int id, UpdateAttendeeDto updateAttendeeDto)
+        {
+            if (id != updateAttendeeDto.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingAttendee = await _attendeeRepository.GetByIdAsync(id);
+
+            if (existingAttendee == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(updateAttendeeDto, existingAttendee);
+
+            await _attendeeRepository.UpdateAsync(existingAttendee);
+
+            return NoContent();
+        }
+
+        // DELETE: api/attendees/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAttendee(int id)
+        {
+            var existingAttendee = await _attendeeRepository.GetByIdAsync(id);
+
+            if (existingAttendee == null)
+            {
+                return NotFound();
+            }
+
+            await _attendeeRepository.DeleteAsync(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ WAD.00014384/Controllers/EventsController.cs
@@ -0,0 +1,92 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WAD._00014384.DAL.DTOs;
+using WAD._00014384.DAL.Interfaces;
+using WAD._00014384.DAL.Models;
+
+namespace WAD._00014384.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly IEventRepository _eventRepository;
+        private readonly IMapper _mapper;
+
+        public EventsController(IEventRepository eventRepository, IMapper mapper)
+        {
+            _eventRepository = eventRepository;
+            _mapper = mapper;
+        }
+
+        // GET: api/events
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EventDto>>> GetEvents()
+        {
+            var events = await _eventRepository.GetAllAsync();
+            return Ok(_mapper.Map<IEnumerable<EventDto>>(events));
+        }
+
+        // GET: api/events/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EventDto>> GetEvent(int id)
+        {
+            var @event = await _eventRepository.GetByIdAsync(id);
+
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return _mapper.Map<EventDto>(@event);
+        }
+
+        // POST: api/events
+        [HttpPost]
+        public async Task<ActionResult<EventDto>> CreateEvent(CreateEventDto createEventDto)
+        {
+            var @event = _mapper.Map<Event>(createEventDto);
+            await _eventRepository.AddAsync(@event);
+            return CreatedAtAction(nameof(GetEvent), new { id = @event.Id }, _mapper.Map<EventDto>(@event));
+        }
+
+        // PUT: api/events/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEvent(int id, UpdateEventDto updateEventDto)
+        {
+            if (id != updateEventDto.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingEvent = await _eventRepository.GetByIdAsync(id);
+
+            if (existingEvent == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(updateEventDto, existingEvent);
+
+            await _eventRepository.UpdateAsync(existingEvent);
+
+            return NoContent();
+        }
+
+        // DELETE: api/events/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEvent(int id)
+        {
+            var existingEvent = await _eventRepository.GetByIdAsync(id);
+
+            if (existingEvent == null)
+            {
+                return NotFound();
+            }
+
+            await _eventRepository.DeleteAsync(id);
+
+            return NoContent();
+        }
+    }
+}
